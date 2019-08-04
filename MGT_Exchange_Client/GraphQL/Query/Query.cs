d395b7cb699e4ff6acea2d268b3cf74118c74286
Pedro Angulo <pedro.angulo.sandoval@outlook.com>
2019-08-03 23:11:04 -0500

    Initial Release

---
+++ MGT_Exchange_Client/GraphQL/Query/Query.cs
@@ -0,0 +1,25 @@
+﻿using MGT_Exchange_Client.GraphQL.MVC;
+using SAHB.GraphQLClient.FieldBuilder.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MGT_Exchange_Client.GraphQL.Query
+{
+    
+    public class QueryCustomerByIds
+    {
+        // Take this information from the Schema in the playground
+        [GraphQLArguments("customerIds", "[ID!]", "customerIdsVar")]
+        //[GraphQLArguments()]
+        public List<Customer> customerByIds { get; set; } //  // This is case sensitive (ReturnType) (QueryName) 
+    }
+
+    public class QueryChat
+    {
+        // Take this information from the Schema in the playground
+        [GraphQLArguments("id", "IntType!", "idVar")]
+        public chat chat { get; set; } //  // This is case sensitive (ReturnType) (QueryName) 
+    }
+
+}
---
+++ MGT_Exchange_Client/GraphQL/Query/QueryChat.cs
@@ -0,0 +1,11 @@
+﻿using MGT_Exchange_Client.GraphQL.MVC;
+using SAHB.GraphQLClient.FieldBuilder.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MGT_Exchange_Client.GraphQL.Query
+{
+
+
+}
