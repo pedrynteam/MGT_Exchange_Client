21e1e2f29a4c90a1db2ef4febee886e69a482a16
Pedro Angulo <pedro.angulo.sandoval@outlook.com>
2019-08-05 21:18:45 -0500

    Version addind a folder Testing to the Client, so when creating calls the user can know exactly how to configure the call

--- MGT_Exchange_ClientNUnitTest/UnitTestMutation.cs
+++ MGT_Exchange_ClientNUnitTest/UnitTestMutation.cs
@@ -2,6 +2,7 @@
 using MGT_Exchange_Client.GraphQL.Mutation;
 using MGT_Exchange_Client.GraphQL.MVC;
 using MGT_Exchange_Client.GraphQL.Query;
+using MGT_Exchange_Client.GraphQL.Testing;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -11,11 +12,7 @@ using System.Threading.Tasks;
 namespace NUnitTest_Exchange
 {
     class UnitTestMutationMin
-    {
-        readonly IMGTExchangeClient clientMGT = new MGTExchangeClient();
-        readonly string _url = "http://10.18.24.67:8082/";
-        readonly string _token = "token";
-
+    {        
         [SetUp]
         public void Setup()
         {
@@ -24,21 +21,8 @@ namespace NUnitTest_Exchange
         [Test]
         public async Task TestMutationCreateCompanyAndXUsersTxn()
         {
-            MutationCreateCompanyAndXUsersTxn_Input input = new MutationCreateCompanyAndXUsersTxn_Input
-            {
-                company = new company
-                {
-                    name = "company9",
-                    description= "one",
-                    email = "one@one.com",
-                    password = "12345"
-                },
-                userstocreate = 10,
-                url = _url,
-                token = _token
-            };
-            
-            MutationCreateCompanyAndXUsersTxn_Output output = await clientMGT.MutationCreateCompanyAndXUsersTxn(input: input);
+            MutationCreateCompanyAndXUsersTxn_Output output =
+                await MGTMutationTest.TestMutationCreateCompanyAndXUsersTxn();
 
             Assert.That(output, Is.Not.Null);
             Assert.That(output.ResultConfirmation.resultPassed, Is.True);
@@ -49,46 +33,9 @@ namespace NUnitTest_Exchange
         [Test]
         public async Task TestMutationCreateChatTxn()
         {
-
-            // Pending, when the user is not from this company return: Inner Exception 
-
-            MutationCreateChatTxn_Input input = new MutationCreateChatTxn_Input
-            {
-                chat = new chat
-                {
-                    chatId = 0,
-                    name = "Chat 1",
-                    companyId = "04c6b67c-6f4d-455f-a472-d6206dd769df",
-                    participants = new List<participant>
-                    {
-                        new participant
-                        {
-                            participantId = 0,
-                            chatId = 0,
-                            isAdmin = true,
-                            userAppId = "10d3ed1e-ae7d-4d0c-80e0-aa360d76025b"
-                        },
-                        new participant
-                        {
-                            participantId = 0,
-                            chatId = 0,
-                            isAdmin = false,
-                            userAppId = "127b0df2-d732-478e-bf84-0b673c48d145"
-                        },
-                        new participant
-                        {
-                            participantId = 0,
-                            chatId = 0,
-                            isAdmin = false,
-                            userAppId = "3982349d-289d-4f3a-ab29-f3f6bb3893f4"
-                        }
-                    },
-                },
-                url = "http://10.18.24.67:8082/",
-                token = "token"
-            };
-            
-            MutationCreateChatTxn_Output output = await clientMGT.MutationCreateChatTxn(input: input);
+         
+            MutationCreateChatTxn_Output output = 
+                await MGTMutationTest.TestMutationCreateChatTxn();
 
             Assert.That(output, Is.Not.Null);
             Assert.That(output.ResultConfirmation.resultPassed, Is.True);
@@ -99,25 +46,13 @@ namespace NUnitTest_Exchange
         [Test]
         public async Task TestMutationAddCommentToChatTxn()
         {
-            MutationAddCommentToChatTxn_Input input = new MutationAddCommentToChatTxn_Input
-            {
-                comment = new comment
-                {                   
-                    chatId= 3,
-                    message= "Hello There",        
-                    userAppId= "10d3ed1e-ae7d-4d0c-80e0-aa360d76025b"
-                },                
-                url = _url,
-                token = _token
-            };
-
-            MutationAddCommentToChatTxn_Output output = await clientMGT.MutationAddCommentToChatTxn(input: input);
+            MutationAddCommentToChatTxn_Output output =
+                await MGTMutationTest.TestMutationAddCommentToChatTxn();
 
             Assert.That(output, Is.Not.Null);
             Assert.That(output.ResultConfirmation.resultPassed, Is.True);
             Assert.That(output.comment, Is.Not.Null);
             Assert.That(output.comment.commentId, Is.GreaterThan(0));
-
         }
 
     }
--- MGT_Exchange_ClientNUnitTest/UnitTestQuery.cs
+++ MGT_Exchange_ClientNUnitTest/UnitTestQuery.cs
@@ -1,11 +1,6 @@
-﻿using MGT_Exchange_Client.GraphQL.Interface;
-using MGT_Exchange_Client.GraphQL.MVC;
-using MGT_Exchange_Client.GraphQL.Query;
+﻿using MGT_Exchange_Client.GraphQL.Query;
+using MGT_Exchange_Client.GraphQL.Testing;
 using NUnit.Framework;
-using SAHB.GraphQLClient;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace NUnitTest_Exchange
@@ -14,28 +9,17 @@ namespace NUnitTest_Exchange
     {
         // https://wrightfully.com/assert-framework-comparison
         
-        readonly IMGTExchangeClient clientMGT = new MGTExchangeClient();
-        readonly string _url = "http://10.18.24.67:8082/";
-        readonly string _token = "token";
-
         [SetUp]
         public void Setup()
         {
         }
 
-
         [Test]
         public async Task TestQueryAllUsersByCompany()
         {
-            QueryAllUsersByCompany_Input input = new QueryAllUsersByCompany_Input
-            {                
-                company = new company { companyId = "04c6b67c-6f4d-455f-a472-d6206dd769df" },
-                url = _url,
-                token = _token
-            };
-                        
-            QueryAllUsersByCompany_Output output = await clientMGT.QueryAllUsersByCompany(input: input);
-            
+            QueryAllUsersByCompany_Output output = 
+                await MGTQueryTest.TestQueryAllUsersByCompany();
+
             Assert.That(output, Is.Not.Null);
             Assert.That(output.ResultConfirmation.resultPassed, Is.True);
             Assert.That(output.company.users.Count, Is.GreaterThan(0));
@@ -44,57 +28,23 @@ namespace NUnitTest_Exchange
         [Test]
         public async Task TestQueryRetrieveRecentChats()
         {
-            // This is to be used as master List, to show recent chats, and recent message of each chat (seen or unseen). 
-            // After that the user can click in an specific chat to check more information
-            QueryRetrieveMasterInformationByUser_Input input = new QueryRetrieveMasterInformationByUser_Input
-            {               
-                UserApp = new userApp { userAppId = "10d3ed1e-ae7d-4d0c-80e0-aa360d76025b" },
-                chatsRecentTake = 5,
-                commentsSeenTake = 0,
-                commentsUnseenTake = 0,
-                commentsNewestTake = 1,
-                commentsBeforeUnseenTake = 0,
-                findSpecificChatId = 0, // 0 means no, any number means ChatId
-                url = _url,
-                token = _token
-            };
-            
-            QueryRetrieveMasterInformationByUser_Output output = await clientMGT.QueryRetrieveRecentChats(input: input);
+            QueryRetrieveMasterInformationByUser_Output output = 
+                await MGTQueryTest.TestQueryRetrieveRecentChats();
 
             Assert.That(output, Is.Not.Null);
             Assert.That(output.ResultConfirmation.resultPassed, Is.True);
             Assert.That(output.commentsNewest.Count, Is.GreaterThan(0));
-
         }
 
         [Test]        
         public async Task TestQueryRetrieveChatComments()
         {
-            // This is to be used when a user clicks any Chat from the master chats list
-            // The app must show unseen comments if any, if none, wold show newest comments
-            // Logic in FrontEnd must be
-            // IF commentsUnseen is null THEN show only commentsNewest
-            // ELSE show commentsUnseen   (Note: In the transaction the beforeUnsee and Unseen are joined)
-            // Pending mark as seen all the commentsUnseen Retrieved, create a variable to decidi if mark as seen or not
-            QueryRetrieveMasterInformationByUser_Input input = new QueryRetrieveMasterInformationByUser_Input
-            {
-                UserApp = new userApp { userAppId = "127b0df2-d732-478e-bf84-0b673c48d145" }, //10d3ed1e-ae7d-4d0c-80e0-aa360d76025b" },
-                chatsRecentTake = 1,
-                commentsSeenTake = 0,
-                commentsBeforeUnseenTake = 5,
-                commentsUnseenTake = 10,
-                commentsNewestTake = 10,                
-                findSpecificChatId = 2, // 0 means no, any number means ChatId
-                url = _url,
-                token = _token
-            };
-
-            QueryRetrieveMasterInformationByUser_Output output = await clientMGT.QueryRetrieveRecentChats(input: input);
+            QueryRetrieveMasterInformationByUser_Output output = 
+                await MGTQueryTest.TestQueryRetrieveChatComments();
 
             Assert.That(output, Is.Not.Null);
             Assert.That(output.ResultConfirmation.resultPassed, Is.True);
             Assert.That(output.commentsNewest.Count, Is.GreaterThan(0));
-
         }
     }
 }
